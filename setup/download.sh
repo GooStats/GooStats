79f7b154125edbe0ac4520d02b95a4cd6547cb09
Xuefeng Ding <xuefeng.ding.physics@gmail.com>
2019-06-27 10:45:15 +0200

    make also link for compile.sh

--- setup/download.sh
+++ setup/download.sh
@@ -38,3 +38,5 @@ export GTEST_ROOT="$(readlink -f googletest-install)"
 export GOOFIT_DIR="$(readlink -f GooFit-install)"
 export CMAKE_PREFIX_PATH=\$GOOFIT_DIR:\$CMAKE_PREFIX_PATH
 EOF
+
+ln -s GooStats/setup/compile.sh
